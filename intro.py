0db995943016fb08a5a33a1ca982b34c18d592ae
prabhanjan27 <149321203+prabhanjan27@users.noreply.github.com>
2023-11-06 13:12:53 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,3 +1,10 @@
+class BracketMapper:
+    def __init__(self):
+        self.brackets = {')': '(', '}': '{', ']': '['}
+
+    def is_match(self, open_bracket, close_bracket):
+        return self.brackets.get(close_bracket) == open_bracket
+        
 class Stack:
     def __init__(self):
         self.items = []
